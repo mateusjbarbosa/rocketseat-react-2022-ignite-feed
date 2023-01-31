d9905bb009e010bd09edd5db601a890341487730
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-01-31 14:16:50 -0300

    refactor: changed avatar to Profile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Profile } from '../Profile';
 import styles from './styles.module.css';
 
 export function Sidebar() {
@@ -9,15 +10,13 @@ export function Sidebar() {
       className={styles.cover}
     />
 
-    <div className={styles.profile}>
-      <img
-        src="https://images.unsplash.com/photo-1599425857670-b17a03c9c897?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb25kZSUyMHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=300&q=30"
-        alt="User's profile picture"
-        className={styles.avatar}
+    <div className={styles.avatar}>
+      <Profile
+        name="Aurora Ribeiro"
+        orientation='vertical'
+        picture="https://images.unsplash.com/photo-1599425857670-b17a03c9c897?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb25kZSUyMHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=300&q=30"
+        role='Front-end Developer'
       />
-
-      <strong>Aurora Ribeiro</strong>
-      <span>Front-end Developer</span>
     </div>
 
     <footer className={styles.footer}>
