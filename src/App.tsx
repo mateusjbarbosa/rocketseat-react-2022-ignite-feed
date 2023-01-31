2db714c63be063e8972c03246eab91320a19b31a
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-01-31 12:30:45 -0300

    feat: updated App with Wrapper and Sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Wrapper } from './components/Wrapper';
 import './global.css';
 
 export function App() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <Wrapper>
+        <Sidebar />
+        <main>posts</main>
+      </Wrapper>
+    </>
   )
 }
