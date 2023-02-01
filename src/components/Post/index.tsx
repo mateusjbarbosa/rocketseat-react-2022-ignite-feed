dc1f7d7e94a32fa25344caaafe90c1c5b73a51a2
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-02-01 11:30:16 -0300

    feat: added comment list in Post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -35,6 +35,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
